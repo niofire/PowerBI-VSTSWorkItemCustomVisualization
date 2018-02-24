01b98eba54b5ffb538cb8cac5dd6fcfc103dc217
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2018-02-24 14:00:52 -0800

    Initial Commit

---
+++ VSTSWorkItemVisualizer/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var vSTSWorkItemVisualizerED03F6FE38634C41BE0B8A4803CBB2C9_DEBUG = {
+        name: 'vSTSWorkItemVisualizerED03F6FE38634C41BE0B8A4803CBB2C9_DEBUG',
+        displayName: 'VSTSWorkItemVisualizer',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.10.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.vSTSWorkItemVisualizerED03F6FE38634C41BE0B8A4803CBB2C9.Visual(options),
+        custom: true
+    };
+}
